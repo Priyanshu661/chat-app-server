6ae23d96b1f0061e7436e15c8c94422eb0ba6a08
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2023-12-25 18:34:42 +0530

    added login api

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcrypt");
 const sequelize = require("../database/db");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res) => {
   const t = await sequelize.transaction();
@@ -51,6 +52,53 @@ const signup = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  const t = await sequelize.transaction();
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.json({
+        success: false,
+        error: "Please provide all the details.",
+      });
+    }
+
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+      transaction: t,
+    });
+
+    if (!user) {
+      return res.json({
+        success: false,
+        error: "Account Not Found!",
+      });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.json({
+        success: false,
+        error: "Invalid Email or Password!",
+      });
+    }
+
+    const token = await jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    await t.commit();
+    return res
+      .status(200)
+      .json({ success: true, token, message: "Login Successfull!" });
+  } catch (e) {
+    await t.rollback();
+    return res.status(400).json({ success: false, error: e });
+  }
+};
 module.exports = {
   signup,
+  login
 };
