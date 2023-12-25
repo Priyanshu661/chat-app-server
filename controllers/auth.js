9923f3d18309ca023cf0a81c32d5abc47cfe3e11
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2023-12-25 17:37:38 +0530

    connected backend to frontend

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,16 @@ const bcrypt = require("bcrypt");
 const sequelize = require("../database/db");
 
 const signup = async (req, res) => {
-      const t = await sequelize.transaction();
+  const t = await sequelize.transaction();
   try {
     const { name, email, phone, password } = req.body;
 
     if (!name || !email || !phone || !password) {
-      return res.status(400).json({ error: "Please Fill All The Details." });
+      return res.json({
+        success: false,
+        error: "Please fill all the details.",
+      });
     }
-
-  
 
     const user = await User.findOne({
       where: {
@@ -22,9 +23,10 @@ const signup = async (req, res) => {
     });
 
     if (user) {
-      return res
-        .status(400)
-        .json({ error: "Account with this email already exists!" });
+      return res.json({
+        success: false,
+        error: "Account with this email already exists!",
+      });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -41,11 +43,11 @@ const signup = async (req, res) => {
       }
     );
 
-    await t.commit()
-    return res.status(200).json({ message: "Account Created!" });
+    await t.commit();
+    return res.status(200).json({ success: true, message: "Account Created!" });
   } catch (e) {
-     await t.rollback();
-    return res.status(400).json({ error: e });
+    await t.rollback();
+    return res.status(400).json({ success: false, error: e });
   }
 };
 
