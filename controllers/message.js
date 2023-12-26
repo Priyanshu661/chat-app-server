47879ea87fa2b93b2114aa5fe4b1db90e385eb0f
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2023-12-26 14:47:24 +0530

    added fetch message api

--- controllers/message.js
+++ controllers/message.js
@@ -1,5 +1,6 @@
 const sequelize = require("../database/db");
 const Message = require("../models/Message");
+const User = require("../models/User");
 
 const send_message = async (req, res) => {
   const t = await sequelize.transaction();
@@ -29,6 +30,31 @@ const send_message = async (req, res) => {
   }
 };
 
+const fetch_messages = async (req, res) => {
+  try {
+    const messages = await Message.findAll({
+      where: {
+        UserId: req.user.id,
+      },
+      attributes:["message"]
+      
+    });
+
+    const data=messages.map((item)=>{
+      return {
+            name:req.user.name,
+            message:item.message
+      }
+    })
+
+
+    return res.status(200).json({ success: true,data});
+  } catch (e) {
+    return res.status(400).json({ success: false, error: "Server Error" });
+  }
+};
+
 module.exports = {
   send_message,
+  fetch_messages
 };
--- routes/message.js
+++ routes/message.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { send_message } = require("../controllers/message");
+const { send_message, fetch_messages } = require("../controllers/message");
 const { authenticate } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.post("/send-message",authenticate, send_message);
+router.get("/fetch", authenticate, fetch_messages);
+
 
 module.exports = router;
